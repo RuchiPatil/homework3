5e50aa23592b491f654976504d4effb0175b8ecc
RuchiPatil <ruchipatil@outlook.com>
2021-07-03 10:58:25 -0400

    some formatting stuff

--- client.py
+++ client.py
@@ -3,30 +3,64 @@
 import socket
 import pickle
 import yaml
+import re
+import pandas as pd
 #----------------------------GLOBAL VALUES
 HOST = '127.0.0.1'
 PORT = 65432
-
+cols = ['Name', 'ID', 'StreetNumber', 'StreetName', 'City', 'Province', 'Country']
 #---------------------------------------------------------------------------------------MENU
-print("hiii")
-mA = 0
-while mA!=1 and mA!=2:
-    mA = int(input("Enter \n1 for Record Creation\n2 for Record Retrieval"))
-    print(mA)
 
+mA = 0
+while True:
+    mA = input("Enter \n1 for Record Creation\n2 for Record Retrieval\n")
+    if mA=='1' or mA=='2':
+        break
+    print("             You can only choose 1 or 2! :)")
+mA = int(mA)
 #if 1 chosen, Record Input
 datasend = [[], 0] #1 means data is record, 2 means data is ID
 
 #------------------------------------------------------------------------ (if 1) RECORD INPUT
 if mA==1:
-    inName = input("Name:")
-    inID = input("ID:")
-    inStNum = input("Street Number: ")
+    print("\n⁛⁛⁛⁛1. Create record⁛⁛⁛⁛\n")
+    #-------------- Name - check alpha
+    while True:
+        inName = input('Name: ')
+        if inName.isalpha():
+            break
+        print("             ERR: letters, please :)")
+    #-------------- ID - check len=6, digit
+    while True:
+        inID = input("ID: ")
+        if inID.isdigit() and len(inID)==6:
+            break
+        print("             ERR: 6-digit ID, please :)")
+    #-------------- StNum - check len <=4, digit
+    while True:
+        inStNum = input("Street Number: ")
+        if inStNum.isdigit() and len(inStNum)<=4:
+            break
+        print("             ERR: upto 4 digits, please :)")
+    #-------------- St Name
     inStName = input("Street Name: ")
+    #-------------- City - check alpha
     inCity = input("City: ")
-    inPost = input("Postal Code: ")
+    #-------------- Postal Code - check re zip format
+    zipc = re.compile(r"\w\d\w\s\d\w\d")
+    while True:
+        inPost = input('Enter Postal Code (A1A 1A1): ')
+        if zipc.match(inPost):
+            break
+        print("             ERR: 'A1A 1A1' formatting, please :)")
+    #-------------- Province
     inProv = input("Province: ")
-    inCountry = input("Country: ")
+    #-------------- Country - check alpha
+    while True:
+        inCountry = input('Country: ')
+        if inCountry.isalpha():
+            break
+        print("             ERR: only letters, please :)")
 
     infoSend = [inName, inID, inStNum, inStName, inCity, inPost, inProv, inCountry]
     #LIST TO BYTES
@@ -34,7 +68,12 @@ if mA==1:
 
 ##------------------------------------------------------------------------ (if 2) ID INPUT
 elif mA==2:
-    retid = input("Retrieval ID: ")
+    print("\n⁛⁛⁛⁛2. Retrieve record⁛⁛⁛⁛\n")
+    while True:
+        retid = input("Retrieval ID: ")
+        if retid.isdigit() and len(retid)==6:
+            break
+        print("             ERR: 6-digit ID, please :)")
     #STR TO BYTES
     dataSend = bytes(str([retid, 2]), 'UTF-8')
 
@@ -45,7 +84,15 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.sendall(dataSend)
     #BYTES TO STRING
     dataR = (s.recv(1024)).decode('UTF-8')
-
-if dataSend[1]==2:
+# ----------------------------------------------------------------------------Print Received Data
+retframe = pd.DataFrame(columns= ['Name', 'ID', 'StreetNumber', 'StreetName', 'City', 'Province', 'Country'])
+if mA==2:
     dataR = yaml.safe_load(dataR)
-print('recieved', repr(dataR))
+    dataR.insert(0, cols)
+    #retframe = pd.DataFrame(dataR, columns=cols)
+    retframe = pd.DataFrame.from_records(dataR)
+    #retframe.axis('off')
+    #retframe.columns = ['Name', 'ID', 'StreetNumber', 'StreetName', 'City', 'Province', 'Country']
+    print(retframe)
+else:
+    print('recieved', repr(dataR))
--- server.py
+++ server.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
-
+# ------------------- LIBRARIES
 import socket
 import pickle
 import yaml
 import pandas as pd
+import os
 import os.path as osp
 import csv
-#--------------------------------------------------------FUNCTIONS
+import sys
+import keyboard
+# <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><> FUNCTIONS
 # FUNC make a csv with fields
 def createCSV(filename):
     if osp.isfile(filename):
@@ -30,34 +33,45 @@ def readCSV(retID, filename):
     retrows = (frame[(frame.ID == retID)]).values.tolist()
     print("retrows:\n", retrows)
     return retrows
-#------------------------------------------------------EOF FUNCTIONS
+# <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><> EOF FUNCTIONS
+#-------------------- GLOBAL VARIABLES
 HOST = '0.0.0.0'
 PORT = 65432
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    affirm = "we got it"
-    with conn:
-        print('connected by', addr)
-        while True:
-            #BYTES to STRING
-            recieved = (conn.recv(1024)).decode('UTF-8')
-            data = yaml.safe_load(recieved)
-            if not data:
-                break
-            #if data[1]==1 -> write data[0] to csv
-            if data[1]==1:
-                #create File with fields or check if its there
-                createCSV('plop.csv')
-                #write data[0] to filename
-                affirm = writeCSV(data[0], 'plop.csv')
-                print(affirm)
-            elif data[1]==2: #else if data[2]==1 -> make id==data[0] query
-                affirm = str(readCSV(str(data[0]), 'plop.csv'))
+print(f"Listening on port {PORT}, indefinitely. \nTo stop server, close terminal.\n")
+#---------------------------------------------------------------- SOCKET CONNECT
+while True:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        #s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind((HOST, PORT))
+        s.listen()
+        conn, addr = s.accept()
+        print("Waiting for requests.\n")
+        with conn:
+            print('connected by', addr)
+            while True:
+                #convert received data from BYTES to STRING
+                recieved = (conn.recv(1024)).decode('UTF-8')
+                data = yaml.safe_load(recieved)
+                if not data:
+                    break
+                #if data[1]==1 -> write data[0] to csv ---------------- (RECORD WRITE)
+                if data[1]==1:
+                    #create File with fields or check if its there
+                    createCSV('plop.csv')
+                    #write data[0] to filename
+                    affirm = writeCSV(data[0], 'plop.csv')
+                    print(affirm)
+                #else if data[2]==1 -> make id==data[0] query-------------- (RECORD READ)
+                elif data[1]==2:
+                    affirm = str(readCSV(str(data[0]), 'plop.csv'))
+                    #print(f"        data: {data}\ntype of data: {type(data)}")
 
-            print(f"        data: {data}\ntype of data: {type(data)}")
+                #--------------- Send confirmation OR READ_RECORD back to client (bytes)
+                conn.sendall(bytes(affirm, 'UTF-8'))
 
-            #Send bytes
-            conn.sendall(bytes(affirm, 'UTF-8'))
+
+
+
+
+#Something
